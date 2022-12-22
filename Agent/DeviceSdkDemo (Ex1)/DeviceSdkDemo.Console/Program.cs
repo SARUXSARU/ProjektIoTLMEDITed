421c53278ebf62073c3d1d60801d3b11530154ca
SARUXSARU <lukasz.muras25@gmail.com>
2022-12-22 18:45:22 +0100

    Add new files

---
+++ Agent/DeviceSdkDemo (Ex1)/DeviceSdkDemo.Console/Program.cs
@@ -0,0 +1,91 @@
+﻿using DeviceSdkDemo.Device;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+using System.Net.Mime;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+
+        string path = Directory.GetCurrentDirectory() + @"\Linki.txt"; // odczyt danych z pliku .txt wymaganych do połączenia z IotHub oraz serwerem maszyny
+        string[] FileContents = File.ReadAllLines(path);
+        string OpcConnectionString = FileContents[3];
+        string deviceConnectionString = FileContents[1];
+        //string deviceConnectionString = "HostName=IOT-HUBzzzz.azure-devices.net;DeviceId=22.10.13;SharedAccessKey=e5U+h/asb5j23T3fGB0SwsWu2pwWkULCS8NEv4ymCpo=";
+        using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+        await deviceClient.OpenAsync();
+        var device = new VirtualDevice(deviceClient);
+
+        Console.WriteLine($"Connection to IoT success!");
+
+        using (var client = new OpcClient(OpcConnectionString))
+        {
+            try      // zabezpieczenie przed błędnie podanym OpcConnectionString lub w sytuacji gdy połączenie nie może zosatć nawiązane  ( program wyświetli odpowiednie komunikaty gdy się połączy lub nie
+            {
+                client.Connect();
+                Console.WriteLine("connection to devices succesed");
+                var periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+                while (await periodicTimer.WaitForNextTickAsync())   // pobieranie danych z maszyny co sekundę
+                {
+                    var node = client.BrowseNode(OpcObjectTypes.ObjectsFolder);        //   ## wyszukiwanie nazwy aktualnie działających device
+                    //Console.WriteLine(node);
+                    foreach (var childNode in node.Children())
+                    {
+                        if ($"{childNode.DisplayName.Value}" != "Server")
+                        {    //Console.WriteLine(childNode.DisplayName.Value);
+                            int x = childNode.DisplayName.Value.Length;
+                            string deviceName = childNode.DisplayName.Value;
+                            string deviceID = "";
+                            for (int y = 7; y < x; y++)
+                            {
+                                deviceID = deviceID + deviceName[y];
+                            }
+                            int.TryParse(deviceID, out int k);                      // ##
+
+                            int ProductionStatus = (int)client.ReadNode($"ns=2;s=Device {k}/ProductionStatus").Value;   // ### przygotowanie danych do wysłania jako danych telemetrycznych
+                            string WorkorderId = (string)client.ReadNode($"ns=2;s=Device {k}/WorkorderId").Value;
+                            long GoodCount = (long)client.ReadNode($"ns=2;s=Device {k}/GoodCount").Value;
+                            long BadCount = (long)client.ReadNode($"ns=2;s=Device {k}/BadCount").Value;
+                            double Temperature = (double)client.ReadNode($"ns=2;s=Device {k}/Temperature").Value;
+                            //Console.WriteLine(deviceName);
+                            dynamic data = new
+                            {
+                                //DeviceID =  deviceName,
+                                ProductionStatus,
+                                WorkorderId,
+                                GoodCount,
+                                BadCount,
+                                Temperature
+                            };                                                                                         // ### 
+                            //Console.WriteLine(data.Temperature);
+
+                            await device.SendMessages(data);    // wysłanie danych telemetrycznych
+
+                            int deviceErrors = (int)client.ReadNode($"ns=2;s=Device {k}/DeviceError").Value;            // #### Przygotowanie danych do przesłania reported deviceTwin
+                            int productionRate = (int)client.ReadNode($"ns=2;s=Device {k}/ProductionRate").Value;       // ####
+
+                            await device.Twin(deviceErrors, productionRate);                                            // Przesłanie danych reported deviceTwin
+
+                            await device.initializehandlers();                                                         // możliwość wysyłania wiadomości lub directMethod do agenta
+
+                        }
+                    }
+                }
+            }
+            catch   // wychwycenie sytuacji gdy nie można nawiązać połączenia z serverm OpcConnectionString
+            {
+                Console.WriteLine("connection devices fault");
+            }
+            client.Disconnect();
+        }
+        Console.ReadLine();
+    }
+}
+
---
+++ Agent/DeviceSdkDemo (Ex1)/DeviceSdkDemo.Device/VirtualDevice.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using System.Net.Mime;
+using System.Text;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+//using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DeviceSdkDemo.Device
+{
+    public class VirtualDevice
+    {
+        private readonly DeviceClient client;
+
+        public VirtualDevice(DeviceClient deviceClient)
+        {
+            this.client = deviceClient;
+        }
+
+        #region Sending Messages
+
+        public async Task SendMessages(dynamic data)
+        {
+            var rnd = new Random();
+
+            Console.WriteLine($"Device sending messages to IoTHub...\n");
+            Console.WriteLine(data);
+            var dataString = JsonConvert.SerializeObject(data);
+
+            Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataString));
+            eventMessage.ContentType = MediaTypeNames.Application.Json;
+            eventMessage.ContentEncoding = "utf-8";
+
+
+            Console.WriteLine($"\t{DateTime.Now.ToLocalTime()}, Data: [{dataString}]");
+
+            await client.SendEventAsync(eventMessage);
+        }
+
+
+
+        #endregion Sending Messages
+
+        #region Device Twin
+
+        public async Task Twin(dynamic deviceErrors, dynamic productionRate)
+        {
+            var twin = await client.GetTwinAsync();
+
+            Console.WriteLine($"\nInitial twin value received: \n{JsonConvert.SerializeObject(twin, Formatting.Indented)}");
+            Console.WriteLine();
+
+            var reportedProperties = new TwinCollection();
+            reportedProperties["DeviceError"] = deviceErrors;
+
+            var reportedProperties2 = new TwinCollection();
+            reportedProperties2["ProductionRate"] = productionRate;
+
+
+            await client.UpdateReportedPropertiesAsync(reportedProperties);
+            await client.UpdateReportedPropertiesAsync(reportedProperties2);
+        }
+
+        private async Task OnDesiredPropertyChanged(TwinCollection desiredproperties, object usercontext)
+        {
+            Console.WriteLine($"\tdesired property change:\n\t{JsonConvert.SerializeObject(desiredproperties)}");
+            Console.WriteLine("\tsending current time as reported property");
+            TwinCollection reportedproperties = new TwinCollection();
+            reportedproperties["datetimelastdesiredpropertychangereceived"] = DateTime.Now;
+
+            await client.UpdateReportedPropertiesAsync(reportedproperties).ConfigureAwait(false);
+        }
+
+        #endregion Device Twin
+
+        #region Receiving Messages
+
+        private async Task OnC2dMessageReceivedAsync(Message receivedMessage, object _)
+        {
+            Console.WriteLine($"\t{DateTime.Now}> C2D message callback - message received with Id={receivedMessage.MessageId}.");
+            PrintMessage(receivedMessage);
+
+            await client.CompleteAsync(receivedMessage);
+            Console.WriteLine($"\t{DateTime.Now}> Completed C2D message with Id={receivedMessage.MessageId}.");
+
+            receivedMessage.Dispose();
+        }
+
+        private void PrintMessage(Message receivedMessage)
+        {
+            string messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+            Console.WriteLine($"\t\tReceived message: {messageData}");
+
+            int propCount = 0;
+            foreach (var prop in receivedMessage.Properties)
+            {
+                Console.WriteLine($"\t\tProperty[{propCount++}> Key={prop.Key} : Value={prop.Value}");
+            }
+        }
+
+        #endregion Receiving Messages
+
+        #region Direct Methods
+
+        private async Task<MethodResponse> SendMessagesHandler(MethodRequest methodRequest, object userContext)
+        {
+            Console.WriteLine($"\tMETHOD EXECUTED: {methodRequest.Name}");
+
+            var payload = JsonConvert.DeserializeAnonymousType(methodRequest.DataAsJson, new { data = default(dynamic) });
+
+            await SendMessages(payload.data);
+
+            return new MethodResponse(0);
+        }
+
+        private static async Task<MethodResponse> DefaultServiceHandler(MethodRequest methodRequest, object userContext)
+        {
+            Console.WriteLine($"\tMETHOD EXECUTED: {methodRequest.Name}");
+
+            await Task.Delay(1000);
+
+            return new MethodResponse(0);
+        }
+
+        #endregion Direct Methods
+        public async Task initializehandlers()
+        {
+            await client.SetReceiveMessageHandlerAsync(OnC2dMessageReceivedAsync, client);
+
+            await client.SetMethodHandlerAsync("sendmessages", SendMessagesHandler, client);
+            await client.SetMethodDefaultHandlerAsync(DefaultServiceHandler, client);
+
+            await client.SetDesiredPropertyUpdateCallback(OnDesiredPropertyChanged, client);
+        }
+    }
+}
+
